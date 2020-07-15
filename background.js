c0c295d4545a97ff74e6c0118ed019cddfc90fcd
muradi123 <36930686+muradi123@users.noreply.github.com>
2020-07-16 01:30:00 +0400

    add files

---
+++ background.js
@@ -0,0 +1,37 @@
+chrome.browserAction.onClicked.addListener(function (){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {message: "cliked"}); 
+   });
+});
+
+
+
+
+chrome.runtime.onMessage.addListener(
+    function({changeIcon}, sender, sendResponse) {
+     
+      if (changeIcon === "true"){
+         chrome.browserAction.setIcon({
+            path: {
+                19: "./images/active16.png",
+                38: "./images/active16.png"
+            }
+      
+          })
+      } 
+});
+
+chrome.runtime.onMessage.addListener(
+  function({changeIcon}, sender, sendResponse) {
+             
+      if (changeIcon === "false"){
+          chrome.browserAction.setIcon({
+            path: {
+                  19: "./images/off-button-16.png",
+                  38: "./images/off-button-16.png"
+                  }
+              
+            })
+        } 
+});
+        
