b2985e798bad7c08c83a2811c180d12ab22e3c01
muradi123 <jamburia.muradi@mail.ru>
2020-07-25 07:45:27 -0700

    update code

--- scripts/background.js
+++ scripts/background.js
@@ -1,38 +1,49 @@
-chrome.browserAction.onClicked.addListener(function (){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, {message: "cliked"}); 
-    chrome.tabs.executeScript(null, {file:"./scripts/request.js"})
-   });
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {browserMessage: "tabCliked"})
+  })
 });
-
-
-
+ 
 
 chrome.runtime.onMessage.addListener(
-    function({changeIcon}, sender, sendResponse) {
-     
-      if (changeIcon === "true"){
-         chrome.browserAction.setIcon({
-            path: {
-                19: "./images/active16.png",
-                38: "./images/active16.png"
-            }
-      
+  function({message, links}, sender, sendResponse) {                      
+     if (message === "dom-loaded"){
+
+      axios({
+        method:'get',
+        url: `https://api.privacymonitor.com/score?q=${links}`
+      })
+      .then(res=>{
+        console.log(res.status),
+        console.log(res.data.score),
+        console.log(res.data.previousScore),
+        chrome.browserAction.setIcon({
+          path: {
+              19: "./images/active16.png",
+              38: "./images/active16.png"
+          }
+    
+        })
+        chrome.storage.local.set({stat:res.status});
+        chrome.storage.local.set({scores:res.data.score});
+        chrome.storage.local.set({previous:res.data.previousScore});
+        console.log('true')
+      })
+      .catch(error=>{
+        if (error.response) {
+        console.log(error.response.status)
+        chrome.storage.local.set({stat:error.response.status});
+        chrome.browserAction.setIcon({
+          path: {
+                19: "./images/off-button-16.png",
+                38: "./images/off-button-16.png"
+                }
+            
           })
-      } 
-});
+         console.log('false')
+        }
+      });
+     }
 
-chrome.runtime.onMessage.addListener(
-  function({changeIcon}, sender, sendResponse) {
-             
-      if (changeIcon === "false"){
-          chrome.browserAction.setIcon({
-            path: {
-                  19: "./images/off-button-16.png",
-                  38: "./images/off-button-16.png"
-                  }
-              
-            })
-        } 
 });
-        
+       
--- scripts/content.js
+++ scripts/content.js
@@ -1,178 +1,209 @@
-chrome.storage.sync.get(['stat'], function(result) {
-  if(result.stat === 200){
-    chrome.runtime.sendMessage({changeIcon: "true"});
-  }
-  if(result.stat ==404){
-    chrome.runtime.sendMessage({changeIcon: "false"});
-  }
-});
 
 
-let notSucsess =
- `<div class="modal-div">
-    <div class="flex-div">
-      <div class="closex">
-        <div class="line-one-qrt"></div>
-        <div class="line-two-qrt"></div>
-     </div>
-     <div class="text-user"> Dear user you can request us to analyze and score the web site</div>
-     <span class= "click-buttonre">Analyze</span>
-    </div>
-  </div>`
-;
- 
+let cleaned_host = location.host;
+
+if(location.host.indexOf('www.') === 0){
+  cleaned_host = location.host.replace('www.','');
+}
+else if(location.host.indexOf('web.') === 0){
+  cleaned_host = location.host.replace('web.','');
+}
+else if(location.host.indexOf('docs.') === 0){
+  cleaned_host = location.host.replace('docs.','');
+}
+else if(location.host.indexOf('m.') === 0){
+  cleaned_host = location.host.replace('m.','');
+}
+else if(location.host.indexOf('ru.') === 0){
+  cleaned_host = location.host.replace('ru.','');
+}
+
+
+console.log(cleaned_host);
+
+
+chrome.runtime.sendMessage({message:"dom-loaded", links:cleaned_host});
+
 chrome.runtime.onMessage.addListener(
-  function({message}, sender, sendResponse) {
-  
-    if (message === "cliked"){
-
-      chrome.storage.local.get(['stat','scores','previous'], function(result) {
-        if(result.stat === 200){
-          let succsesHtml = `
-          <div class="sucsses-main-qrt">
-          <div class="first-div-qrt">
-          <div class="first-div-main-qrt">
-          <div class="privacy-text-qrt">privacy<span style="color:#336d88; font-weight:bolder">monitor</span></div>
-          <div class="close-qrt">
-          <div class="line-one-qrt"></div>
-          <div class="line-two-qrt"></div>
-          </div>
-          </div>
-          </div>
-          <div class="second-div-qrt">
-          <div class="site-name-qrt">${cleaned_host}</div>
-          <div class="bottom-div-qrt">
-          <canvas id="circle"></canvas>
-          <div class="main-info-qrt">
-          <div class="scores-qrt">scores:${result.scores}</div>
-          <div>
-          <p class="pCol"> </p>
-          <p class="range">previous scores</p>
+  function({browserMessage}, sender, sendResponse) {
+    if (browserMessage === "tabCliked"){  
+      chrome.storage.local.get(['stat', 'scores', 'previous', 'domain' ], function(result) {
+      if(result.scores && result.stat === 200){
+        let succsesHtml = `
+        <div class="mtdfvf-vdfgvd-vgnnbr-345g">
+         <div class="mtfds-vdvdf-34yhn-dvvd">
+         <div class="mtrtr-vdvdfg-re43-gfdgre">privacy<span style="color:#336d88; font-weight:bolder">monitor</span></div>
+         <div class="mtrg-fsfs-432-34tg-43t3">
+         <div class="mtdfd-gdf-dfbdfb-45334fv-vdvd"></div>
+         <div class="mt-dfgd-gdfgd-34fvbc-bfd"></div>
          </div>
          </div>
+         <div class="mtfd-dvfg-dfbtn-bgfb"></div>
+         <div>
+         <div class="mtv-fgdgb-y65-gh4">${cleaned_host}</div>
+         </div>
+         <div class="mtcs-fsvsv-bfdgbfg">
+         <canvas id="mtsdsf-bfvdf-4534bg-dfbf"></canvas>
+         <div class="mtbfgb-htyht-45ggfb">
+         <div class="mgdf-gdg-dgfdh">scores:${result.scores}</div>
+         <div>
+         <div class="pCol"></div>
+         <div class="mtvd-dgfd-fdvd-gerg">
+         <div>trend:</div>
+         <div class="mtfvv-gdgdf-dfvd">
          </div>
          </div>
-         </div>`;
+        </div>
+        </div>
+         </div>
+        </div>
+       `;
 
-          console.log('true')
-          document.body.innerHTML += succsesHtml;
-          $('.sucsses-main-qrt').nextAll('div').remove();
-          if(result.scores===result.previous){
-            console.log('same')
-            $('.range').text('No change');
-            $('.range').css('color','#336d88')
-          }
-          
-          if(result.scores > result.previous){
-           console.log('increase')
-           $('.range').text('Increase');
-           $('.range').css('color','#ade498')
-         }
-         if(result.scores > result.previous){
-           console.log('Decrease')
-           $('.range').text('Decrease');
-           $('.range').css('color','e84a5f')
-         }
-          let canvas = document.getElementById("circle");
-          let widthC = "120";
-          let heightC="120";
-          canvas.width = widthC;
-          canvas.height = heightC;
-          let ctx = canvas.getContext("2d");
-          let percent = result.scores/10;
-          
-          ctx.beginPath();
-          ctx.arc(widthC/2,heightC/2, 45, 0, Math.PI * 2);
-          ctx.strokeStyle = "#f7fbe1";
-          ctx.lineWidth = 8;
-          ctx.stroke();
-          ctx.closePath();
-          
-          let angle = percent/100*360;
-          ctx.beginPath();
-          ctx.arc(widthC/2, heightC/2, 45, -90 * Math.PI/180, (angle - 90) * Math.PI/180);
-          if(percent > 30 && percent < 49){
-            ctx.strokeStyle = "#fe91ca";
-            $('.pCol').text('Very Poor')
-            $('.pCol').css('color', '#fe91ca')
-           }
-          if(percent > 50 && percent < 69){
-            ctx.strokeStyle = "#df5e88";
-            $('.pCol').text('Fair')
-            $('.pCol').css('color', '#df5e88')
-           }
-           if(percent > 70 && percent < 79){
-            ctx.strokeStyle = "#6a2c70";
-            $('.pCol').text('Good')
-            $('.pCol').css('color', '#6a2c70')
-           }
-           if(percent > 80 && percent < 89){
-            ctx.strokeStyle = "#336d88";
-            $('.pCol').text('Very Good')
-            $('.pCol').css('color', '#336d88')
-           }
-           if(percent > 90 && percent < 99){
-            ctx.strokeStyle = "#092532";
-            $('.pCol').text('Exceptional')
-            $('.pCol').css('color', '#092532')
-           }
-          ctx.lineWidth = 8;
-          ctx.stroke();
-          ctx.closePath();
-          ctx.textBaseline = "middle";
-          ctx.textAlign = "center";
-          ctx.font = "40px arial bold";
-          ctx.fillStyle = "#fff"
-                     
-
-
-          $('.close-qrt').click(function(){
-            $(this).parent().parent().parent().remove();
-           });
-         
+        console.log('true')
+        document.body.innerHTML += succsesHtml;
+        $('.mtrg-fsfs-432-34tg-43t3').click(function(){
+          $(this).parent().parent().remove();
+         });
+        $('.mtdfvf-vdfgvd-vgnnbr-345g').nextAll('div').remove();
+        if(result.scores===result.previous){
+          console.log('same')
+          $('.mtfvv-gdgdf-dfvd').text('No change');
+          $('.mtfvv-gdgdf-dfvd').css('color','#336d88')
         }
-        else{
-            console.log('false')
-            document.body.innerHTML += notSucsess;
-            $('.modal-div').nextAll('div').remove();
-            $('.closex').click(function(){
-              $(this).parent().parent().remove();
-            })
-            $('.click-buttonre').click(function(){
-            $(this).remove();
-
-            axios({
-              method:'get',
-              url: `https://api.privacymonitor.com/score?q=${cleaned_host}`
-            })
-            .then(res=>{
-              document.body.innerHTML += succsesHtml;
-              $('.sucsses-main-qrt').nextAll('div').remove();
-              
-            })
-            .catch(error=>{
-
-              let div = document.createElement('div');
-              let divText = $(div).text('request sent');
-              $('.text-user').html(divText);
-              setTimeout(() =>{
-              $('.modal-div').remove();
-              },2000)
-               
-                
-            });
-            
-
-            });   
-           }
-        });
+        
+        if(result.scores > result.previous){
+         console.log('increase')
+         $('.mtfvv-gdgdf-dfvd').text('Increase');
+         $('.mtfvv-gdgdf-dfvd').css('color','#79d70f')
+       }
+       if(result.scores < result.previous){
+         console.log('Decrease')
+         $('.mtfvv-gdgdf-dfvd').text('Decrease');
+         $('.mtfvv-gdgdf-dfvd').css('color','#e84a5f')
+       }
+        let canvas = document.getElementById("mtsdsf-bfvdf-4534bg-dfbf");
+        let widthC = "120";
+        let heightC="120";
+        canvas.width = widthC;
+        canvas.height = heightC;
+        let ctx = canvas.getContext("2d");
+        let percent = result.scores/10;
+        
+        ctx.beginPath();
+        ctx.arc(widthC/2,heightC/2, 45, 0, Math.PI * 2);
+        ctx.strokeStyle = "#e4e3e3";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.closePath();
+        
+        let angle = percent/100*360;
+        ctx.beginPath();
+        ctx.arc(widthC/2, heightC/2, 45, -90 * Math.PI/180, (angle - 90) * Math.PI/180);
+        if(percent > 30 && percent < 49){
+          ctx.strokeStyle = "#f54291";
+          $('.pCol').text('Very Poor')
+          $('.pCol').css('color', '#f54291')
+         }
+        if(percent > 50 && percent < 69){
+          ctx.strokeStyle = "#be5683";
+          $('.pCol').text('Fair')
+          $('.pCol').css('color', '#be5683')
+         }
+         if(percent > 70 && percent < 79){
+          ctx.strokeStyle = "#6a197d";
+          $('.pCol').text('Good')
+          $('.pCol').css('color', '#6a197d')
+         }
+         if(percent > 80 && percent < 89){
+          ctx.strokeStyle = "#1b6ca8";
+          $('.pCol').text('Very Good')
+          $('.pCol').css('color', '#1b6ca8')
+         }
+         if(percent > 90 && percent < 99){
+          ctx.strokeStyle = "#1f4068";
+          $('.pCol').text('Exceptional')
+          $('.pCol').css('color', '#1f4068')
+         }
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.closePath();
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        ctx.font = "40px arial bold";
+        ctx.fillStyle = "#fff";
+                  
+      console.log('work')
       }
+      else{
+        let notSucsess =
+        `<div class="mteece-434ggf-gbfgd-jmh">
+         <div class="mtefsd-3rfdv3-fdvdv-asdc">
+           <div class="mtxcd-dsdsgr-343dss-334fd">
+           <div class="mtrtr-vdvdfg-re43-gfdgre">privacy<span style="color:#336d88; font-weight:bolder">monitor</span></div>
+           <div class="mtrg-fsfs-432-34tg-43t3">
+           <div class="mtdfd-gdf-dfbdfb-45334fv-vdvd"></div>
+           <div class="mt-dfgd-gdfgd-34fvbc-bfd"></div>
+        </div>
+           </div>
+           <div class="effvd-42dfdvd-3dss-vdbd"></div>
+           <div class="fvxvdf-gdfa-bgdbfv-asdsd">
+           Sorry, we haven't reviewed ${cleaned_host} yet.<br/>
+           <div class="fcsdf-dffv-34rdf-43trf"></div>
+           If you would like us to add it to the queue for our legal experts to review, hit the "Request" button now.
+           </div>
+           <button id="fsfd-bbrdfb4-g45t-45t3-34gf">Request</button>
+         </div>
+        </div>
+        `
+       ;
+       console.log('false')
+       document.body.innerHTML += notSucsess;
+       if(result.domain === this.window.location.hostname){
+   
+        console.log(result.domain)
+    
+        $('.fvxvdf-gdfa-bgdbfv-asdsd').html('Your request has been received. Please allow up to 10 business days for this site to be reviewed.');
+        $('#fsfd-bbrdfb4-g45t-45t3-34gf').remove();
+        $('.mteece-434ggf-gbfgd-jmh').css('height','150px');
+        setTimeout(()=>{
+          $('.mteece-434ggf-gbfgd-jmh').remove();
+        },2000)
+             
+      }
+       $('.mteece-434ggf-gbfgd-jmh').nextAll('div').remove();
+       $('.mtrg-fsfs-432-34tg-43t3').click(function(){
+         $(this).parent().parent().parent().remove();
+       });
+     
+       $('#fsfd-bbrdfb4-g45t-45t3-34gf').click(function(){
+        chrome.storage.local.set({domain:cleaned_host});
+       $(this).remove();
+       axios({
+         method:'get',
+         url: `https://api.privacymonitor.com/score?q=${cleaned_host}`
+       })
+       .then(res=>{
+         document.body.innerHTML += succsesHtml;
+         $('.sucsses-main-qrt').nextAll('div').remove();
+         
+       })
+       .catch(error=>{
+         let div = document.createElement('div');
+         let divText = $(div).text('request sent');
+         $('.fvxvdf-gdfa-bgdbfv-asdsd').html(divText);
+         setTimeout(() =>{
+         $('.mteece-434ggf-gbfgd-jmh').remove();
+         },2000)
+          
+           
+       });
+       
 
-});
-
-
-
-
-
+       });   
+      }
+    })
+    
+  }
+})
 
 
